94fad5f753e65b17417c1f1b5f13eb0fb594df26
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-08 10:03:44 -0300

    Iniciando projeto FOGE-FOGE

---
+++ fogefoge.c
@@ -0,0 +1,12 @@
+/*||||||||-Autor : Daniel Caetano de Souza Ferreira|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||||||||||-Jogo Criado acompanhando as aulas no Curso do site ALURA: C III: Recursos avançados da linguagem|||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("INICIANDO PROJETO JOGO FOGE-FOGE");
+}
