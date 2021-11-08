3fe5b40f1a95a3a0e43488385247f6d93d330d4a
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-08 11:33:21 -0300

    Dividindo em Funções o main

--- fogefoge.c
+++ fogefoge.c
@@ -5,14 +5,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "fogefoge.h"
 
 int linhas;
 int colunas;
 char **mapa;
 
-int main()
+void lemapa()
 {
-
     FILE *f;
     f = fopen("mapa.txt", "r");
     if (!f)
@@ -20,30 +20,45 @@ int main()
         printf("Erro na leitura do MAPA");
         exit(1);
     }
-
     fscanf(f, "%d %d", &linhas, &colunas);
-    printf("\nLinhas |%d| - Colunas|%d|\n", linhas, colunas);
 
+    alocamapa();
+
+    printf("\n\n");
+    for (int i = 0; i < 5; i++)
+    {
+        fscanf(f, "%s", mapa[i]);
+    }
+
+    fclose(f);
+}
+
+void alocamapa()
+{
     mapa = malloc(sizeof(char *) * linhas);
     for (int i = 0; i < linhas; i++)
     {
         mapa[i] = malloc(sizeof(char) * (colunas + 1));
     }
+}
 
-    for (int i = 0; i < 5; i++)
+void liberamapa()
+{
+    for (int i = 0; i < linhas; i++)
     {
-        fscanf(f, "%s", mapa[i]);
+        free(mapa[i]);
     }
+    free(mapa);
+}
+
+int main()
+{
+    lemapa();
 
     for (int j = 0; j < linhas; j++)
     {
         printf("%s\n", mapa[j]);
     }
 
-    fclose(f);
-    for (int i = 0; i < linhas; i++)
-    {
-        free(mapa[i]);
-    }
-    free(mapa);
+    liberamapa();
 }
